eca127675e8833e57b3b9d6f36f2e7d2ce08c865
moraswi2 <moraswip97@gmail.com>
2024-12-10 19:58:21 +0200

    http

---
+++ lib/http/shared/apiService.dart
@@ -0,0 +1,94 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+import 'httpService.dart';
+import 'dart:io';
+
+class ApiService {
+  HttpService httpService = HttpService();
+
+  //login
+  Future<dynamic> login(String email, String password) async {
+    final endpoint = 'login';
+    final data = {'email': email, 'password': password};
+    return await httpService.post(endpoint, data);
+  }
+
+  //register
+  Future<dynamic> register(String fullName, String phoneNumber, String email, String password, String role) async {
+    final endpoint = 'register';
+    final data = {
+      'fullName': fullName,
+      'phoneNumber': phoneNumber,
+      'email': email,
+      'password': password,
+      'role': role
+    };
+    return await httpService.post(endpoint, data);
+  }
+
+  //changePassword
+  Future<dynamic> changePassword(int userId, String string, String newPassword) async {
+    final endpoint = 'register';
+    final data = {
+      'userId': userId,
+      'string': string,
+      'newPassword': newPassword,
+    };
+    return await httpService.post(endpoint, data);
+  }
+
+  //deleteProfile
+  Future<dynamic> deleteProfile(int id) async {
+    final endpoint = 'register/$id';
+    return await httpService.delete(endpoint);
+  }
+
+  //getOffices
+  Future<dynamic> getOffices() async {
+    final endpoint = 'offices';
+    return await httpService.get(endpoint);
+  }
+
+  //getStores
+  Future<dynamic> getStores(int officeId) async {
+    final endpoint = 'shop/$officeId';
+    return await httpService.get(endpoint);
+  }
+
+
+  //getStoreMenuByCategoryId
+  Future<dynamic> getStoreMenuByCategoryId(int categoryId) async {
+    final endpoint = 'store-menu/category/$categoryId';
+    return await httpService.get(endpoint);
+  }
+
+  //getStoreMenuPromotionMeals
+  Future<dynamic> getStoreMenuPromotionMeals(int storeId) async {
+    final endpoint = '/store-menu/promotion-meals/$storeId';
+    return await httpService.get(endpoint);
+  }
+
+  //getStoreMenuTopMeal
+  Future<dynamic> getStoreMenuTopMeals(int storeId) async {
+    final endpoint = 'store-menu/top-meals/$storeId';
+    return await httpService.get(endpoint);
+  }
+
+  //getStoreMenuCategories
+  Future<dynamic> getStoreMenuCategories(int storeId) async {
+    final endpoint = 'menu-category/$storeId';
+    return await httpService.get(endpoint);
+  }
+
+  //getStoreMenuCategories
+  Future<dynamic> getUserAddress(int userid) async {
+    final endpoint = 'addresses/$userid';
+    return await httpService.get(endpoint);
+  }
+
+  //getStoreMenuCategories
+  Future<dynamic> getOrders(int userid) async {
+    final endpoint = 'order/user/$userid';
+    return await httpService.get(endpoint);
+  }
+}
---
+++ lib/http/shared/httpService.dart
@@ -0,0 +1,70 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class HttpService {
+  String baseUrl = 'https://localhost:7067/api/';
+  String apiKey = '';
+
+  // HttpService({required this.baseUrl, required this.apiKey});
+
+  Future<dynamic> get(String endpoint) async {
+    final url = '$baseUrl$endpoint';
+    final response = await http.get(
+      Uri.parse(url),
+      // headers: {'ApiKey': apiKey},
+    );
+
+    return response;
+  }
+
+  // when submitting object
+  Future<dynamic> post(String endpoint, Map<String, dynamic> data) async {
+    final url = '$baseUrl$endpoint';
+
+    final response = await http.post(Uri.parse(url),
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
+          // 'ApiKey': apiKey
+        },
+        body: jsonEncode(data));
+
+    return response;
+  }
+
+  // when submitting array
+  Future<dynamic> postList(String endpoint, List<dynamic> dataList) async {
+    final url = '$baseUrl$endpoint';
+
+    final response = await http.post(Uri.parse(url),
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
+          // 'ApiKey': apiKey
+        },
+        body: jsonEncode(dataList));
+
+    return response;
+  }
+
+  Future<dynamic> put(String endpoint, Map<String, dynamic> data) async {
+    final url = '$baseUrl$endpoint';
+    final response = await http.put(
+      Uri.parse(url),
+      // headers: {'ApiKey': apiKey},
+      body: data,
+    );
+
+    return response;
+  }
+
+  Future<dynamic> delete(String endpoint) async {
+    final url = '$baseUrl$endpoint';
+    final response = await http.delete(
+      Uri.parse(url),
+      // headers: {'ApiKey': apiKey},
+    );
+
+    return response;
+  }
+}
