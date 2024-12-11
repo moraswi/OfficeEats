e68998851315261d6bdf975c67e31275ad0c80ef
moraswi2 <moraswip97@gmail.com>
2024-12-11 20:45:07 +0200

    auth service skeleton

--- lib/http/authApiService.dart
+++ lib/http/authApiService.dart
@@ -5,12 +5,45 @@ import 'package:eats/http/shared/apiService.dart';
 class AuthApiService {
 
   //login
-  Future<void> login( String email, String password) async {
+  Future<void> loginReq( String email, String password) async {
     try {
 
     } catch (e) {
 
-      print('Login Error: $e');
+      print('loginReq Error: $e');
+      rethrow;
+    }
+  }
+
+  //registerReq
+  Future<void> registerReq(String fullName, String phoneNumber, String email, String password, String role) async {
+    try {
+
+    } catch (e) {
+
+      print('registerReq Error: $e');
+      rethrow;
+    }
+  }
+
+  //changePasswordReq
+  Future<void> changePasswordReq(int userId, String string, String newPassword) async {
+    try {
+
+    } catch (e) {
+
+      print('changePasswordReq Error: $e');
+      rethrow;
+    }
+  }
+
+  //deleteProfileReq
+  Future<void> deleteProfileReq(int id) async {
+    try {
+
+    } catch (e) {
+
+      print('deleteProfileReq Error: $e');
       rethrow;
     }
   }
